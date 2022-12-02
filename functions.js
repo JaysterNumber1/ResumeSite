6e0847015c35e7f8daf748725dfa45b5df93e3c0
Jarrett S <jaysternumber1@gmail.com>
2022-12-02 10:57:38 -0700

    Init

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+function goToHome(){
+    window.location.href = "index.html"
+}
+function goToAbout(){
+    window.location.href = "aboutMe.html"
+}
+function goToComputer(){
+    window.location.href = "computerTalent.html"
+}
+function goToCAD(){
+    window.location.href = "cadDesign.html"
+}
+function goToContact(){
+    window.location.href = "contact.html"
+}
